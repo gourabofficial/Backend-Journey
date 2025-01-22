1b3b9cc04953493999a44691c19852dc2d6357ea
gourabofficial <gangulygourab3@gmail.com>
2025-01-22 15:11:21 +0530

    almost done , now i work on authontication

--- MiniProject/app.js
+++ MiniProject/app.js
@@ -17,6 +17,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+//profile route
+
+app.get('/profile',isLogin, (req, res) => {
+  res.render('login');
+});
+
+//logout route
+
+app.get('/logout', (req, res) => {
+  res.cookie("token", "")
+  res.send('logout successfully');
+
+});
+
+
 app.post('/register', async (req, res) => {
   let { email, password, username, name, age } = req.body;
 
@@ -35,16 +54,44 @@ app.post('/register', async (req, res) => {
         password: hash,
       })
 
-      let token = jwt.sign({  email:email,userid:user._id}, "ronaldo");
+      let token = jwt.sign({ email: email, userid: user._id }, "ronaldo");
       res.cookie('token', token);
       res.send("Registerd Successfully");
 
     })
-
-
-
   });
 });
+
+// login route 
+
+app.post('/login', async (req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(400).send('Somthing went wrong');
+  }
+
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) {
+      let token = jwt.sign({ email: email, userid: user._id }, "ronaldo");
+      res.cookie('token', token);
+      res.send("Login Successfully");
+    } else {
+      res.redirect('/login')
+    }
+  });
+})
+
+function isLogin(req, res, next) {
+  if (req.cookies.token === "") return res.send('You must be login');
+  else {
+    let data = jwt.verify(req.cookies.token, "ronaldo");
+    req.user = data;
+    next();
+  }
+}
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
